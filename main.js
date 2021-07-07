6e8e208f219617575cb38db5e926f8ab98483eda
KarlNersisyan <karlennersisyan814@gmail.com>
2021-07-07 19:28:23 +0400

    has been added tailwind.css file and changes in index.html file

--- main.js
+++ main.js
@@ -1,6 +1,2 @@
-import './style.css'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hi There</h1>
-  <a href="https://karlennersisyan.github.io/DOM_Project/" target="_blank">My WebSite</a>
-`
